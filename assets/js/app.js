4695750ba32ca948b1eb5acb27fcd439b2932350
devs.hashtag <devs.hashtag@gmail.com>
2021-12-05 06:28:33 +0330

    header responsive completed

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+
+const navList = document.getElementById("nav__list");
+const navToggle = document.getElementById("nav__toggle");
+
+// navbar toggle
+navToggle.addEventListener("click", () => {
+    // switch nav toggle and show menu
+    navToggle.classList.toggle('on');
+    navList.classList.toggle("show");
+});
+
+
